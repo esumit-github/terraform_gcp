c3bffd5a04b508a022ecc278ed232e7ac5d1397d
Sumit <2008.sumit@gmail.com>
2021-07-04 21:23:50 +0530

    changes made in machine type, image ubuntu 1805 lts used

--- gcp_vm_instance_code.tf
+++ gcp_vm_instance_code.tf
@@ -1,17 +1,17 @@
 // Configure the Google Cloud provider
 provider "google" {
-  credentials = file("sumit-gcp-278301-e434e7b64189.json")
+  credentials = file("sumit-gcp-278301-9802bc26fa16.json")
   project = "sumit-gcp-278301"
   region  = "asia-east1"
   zone    = "asia-east1-a"
 }
 resource "google_compute_instance" "vm_instance" {
   name         = "terraform-instance"
-  machine_type = "f1-micro"
+  machine_type = "e2-standard-2"
 
   boot_disk {
     initialize_params {
-      image = "debian-cloud/debian-9"
+      image = "ubuntu-os-cloud/ubuntu-1804-lts"
     }
   }
 
